d15ce0cd525344b0efebbd16fc9bdee0b8f41f08
Fabrice Dreux <fdreux@baylibre.com>
2016-07-26 14:22:20 +0200

    Add option branch to kci_get_latest.py to filter the branch we want to check Modif kci_get_latest.py to be able to call it from an other python script modif lava-report.py for measurment.
    Signed-off-by: Fabrice Dreux <fdreux@baylibre.com>

--- kci_get_latest.py
+++ kci_get_latest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 """Return the list of latest kernels tags."""
+import sys,os
 
 import urlparse
 import json
@@ -9,25 +10,16 @@ import requests
 from lib import configuration
 
 def get_latest_tags(config, kernel, job, defconfig_full, limit=1):
-
     """Return the list of latest kernels tags."""
 
-    if config.get('token'):
-        headers = {
+    headers = {
             'Authorization': config.get('token')
         }
-    else:
-        print "No token found in config, bailing out."
-        exit(1)
-
-    if config.get('api') is None:
-        print "No api found in config, bailing out."
-        exit(1)
 
     query = "?sort=created_on&sort_order=-1"
 
     if kernel is not None:
-        query = '?kernel=%s' % kernel
+        query = '&kernel=%s' % kernel
     if job is not None:
         query += '&job=%s' % job
 
@@ -38,42 +30,59 @@ def get_latest_tags(config, kernel, job, defconfig_full, limit=1):
     response = requests.get(api_url, headers=headers)
     data = json.loads(response.content)
 
-    if response.status_code == 404:
-        print "ERROR: page not found"
-        exit(1)
-    if response.status_code == 403:
-        print "ERROR: access forbidden"
-        exit(1)
-    if response.status_code == 500:
-        print "ERROR: internal database error"
+    if response.status_code >= 400:
+        print data
         exit(1)
 
     tags = []
     for result in data['result']:
-        tag = result['kernel']
+        tag = os.path.join(result['job'],result['kernel'])
+
         if tag not in tags:
             tags.append(tag)
 
     return tags
 
-
-def main(args):
-
+def run(args):
     """Query the list of latest kernels tags."""
 
     config = configuration.get_config(args)
+    if config.get('token') is None: 
+        raise ValueError("No token found in config")
+    if config.get('api') is None: 
+        raise ValueError("No api found in config")
 
-    tags = get_latest_tags(config, None, "next", "allmodconfig")
+    tags = get_latest_tags(config, None, config.get('branch'), "allmodconfig")
 
-    print tags[0]
+    return tags
 
-    exit(0)
+
+def kci_get_latest(api,token,config=None,section=None,branch=None):
+    args = {'api':api,'token':token,'config':config,'section':section,'branch':branch}
+    
+    try:
+        return run(args)
+    except Exception, err:
+        print "### ERROR ###", str(err)
+        return 1
+    
+
+def main(args):
+
+    parser = argparse.ArgumentParser(description='Get latest kernel build from kernelci.org')
+    parser.add_argument('-c','--config',  dest='config', help="Configuration for the LAVA server")
+    parser.add_argument('-s','--section', dest='section',help="Section in .lavarc")
+    parser.add_argument('-b','--branch',  dest='branch', help="Branch to check (next, mainline,etc...")
+    required = parser.add_argument_group('MANDATORY argument')
+    required.add_argument('-a','--api',     dest='api',   required=True, help="API url")
+    required.add_argument('-t','--token',   dest='token', required=True, help="Authentication token")
+    args = vars(parser.parse_args())
+
+    try:
+        print run(args)[0]
+    except Exception, err:
+        print "### ERROR ###", str(err)
+        sys.exit(1)
 
 if __name__ == '__main__':
-    PARSER = argparse.ArgumentParser()
-    PARSER.add_argument("--config", help="configuration for the LAVA server")
-    PARSER.add_argument("--section", help="section in .lavarc")
-    PARSER.add_argument("--api", help="api url")
-    PARSER.add_argument("--token", help="authentication token")
-    ARGS = vars(PARSER.parse_args())
-    main(ARGS)
+    main(sys.argv[1:])
