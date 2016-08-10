af4172dc136cc3b8031623828651567ec579e277
Fabrice Dreux <fdreux@baylibre.com>
2016-08-10 09:51:59 +0200

    Some cosmetic modif, add comments
    Signed-off-by: Fabrice Dreux <fdreux@baylibre.com>

--- lava-matching-report.py
+++ lava-matching-report.py
@@ -26,30 +26,8 @@ ATTACH_FILE = "data.csv"
 MATCHING_BOOTS = "matching-boots.json"
 MATCHING_TESTS = "matching-tests.json"
 
-def main(args):
-    config = configuration.get_config(args)
-    url = utils.validate_input(config.get("username"),
-                               config.get("token"), config.get("server"))
-    connection = utils.connect(url)
 
-    bundle_stream = None
-    if config.get("stream"):
-        bundle_stream = config.get("stream")
-
-    bundles = connection.dashboard.bundles(bundle_stream)
-
-    folder = os.getcwd() + '/results'
-    utils.mkdir(folder)
-
-    matching = None
-    if args.has_key("matching"):
-        matching = args['matching']
-        print "Searching bundles named after '%s'..." % matching
-
-    generate_matching_boots(config, connection, bundles, folder, matching)
-    
-
-
+################################################################################
 def generate_matching_boots(config, connection, bundles, folder, matching):
 
     powerci_json = {}
@@ -58,33 +36,34 @@ def generate_matching_boots(config, connection, bundles, folder, matching):
     powerci_json['server'] = config.get("server")
     powerci_json['duration'] = 0
 
-    total_bundles = len(bundles)
-
+    nbr_suitable = 0
     for bundle in bundles:
         if bundle['associated_job'] == "NA":
             bundles.remove(bundle)
             continue
 
-        if matching is not None:
-            if matching not in bundle['content_filename']:
-#                print "remove" + str(bundle['content_filename'])
-                bundles.remove(bundle)
-                continue
+        if matching and matching not in bundle['content_filename']:
+            bundles.remove(bundle)
+            continue
 
         json_bundle = connection.dashboard.get(bundle['content_sha1'])
-        bundle_data = json.loads(json_bundle['content'])
+        #json_bundle is dict() with keys = ['content', 'content_filename']
 
-        number = bundle["associated_job"]
+        bundle_data = json.loads(json_bundle['content'])
+        #bundle_data is dict() with keys = [u'test_runs', u'format']
+
+        job_id = bundle["associated_job"]
 
         # A suitable job:
         # - has a uuid that matches a storage folder under
-        # /var/www/images/kernel-ci/attachments/
+        # /var/www/images/kernel-ci/attachments/<uuid>
         # - could upload a non-empty CSV file with the power measurements
-        # - has power metrics is it's test_run metadata
+        # - has power metrics in its test_run metadata
         #
         found_suitable = False
-
         for test_results in bundle_data['test_runs']:
+            #test_results is dict() with keys = [u'attachments', u'analyzer_assigned_date', u'time_check_performed', u'test_results', u'analyzer_assigned_uuid', u'attributes', u'test_id']
+            #    test_results['test_id'] can be 'lava-command','lava'
             if test_results['test_id'] == 'lava-command':
                 uuid = test_results['analyzer_assigned_uuid'].split('-', 1)[0]
                 attach_dir = os.path.join(ATTACH_FOLDER, uuid)
@@ -105,26 +84,57 @@ def generate_matching_boots(config, connection, bundles, folder, matching):
                 for test in test_results['test_results']:
                     if test['test_case_id'] in POWER_METRICS:
                         found_suitable = True
+                        break
 
         if not found_suitable:
             bundles.remove(bundle)
             continue
 
+        print "Found matching job %d" % job_id
+        nbr_suitable += 1
         data = {}
         data['bundle'] = bundle['content_sha1']
         data['result'] = "PASS"
-        powerci_json[number] = data
+        powerci_json[job_id] = data
 
+    print "  => Found %d suitable job" % nbr_suitable
     utils.write_json(MATCHING_BOOTS, folder, powerci_json)
 
+################################################################################
+def main(args):
+    config = configuration.get_config(args)
+    url = utils.validate_input(config.get("username"),
+                               config.get("token"), config.get("server"))
+    connection = utils.connect(url)
+
+    bundle_stream = None
+    if config.get("stream"):
+        bundle_stream = config.get("stream")
+
+    print "Get all bundles"
+    bundles = connection.dashboard.bundles(bundle_stream)
+    print "  => %d bundles to analyse" % len(bundles)
+
+    folder = os.path.join(os.getcwd(),'results')
+    utils.mkdir(folder)
+
+    matching = None
+    if args.has_key("matching"):
+        matching = args['matching']
+        print "Searching  suitable bundles named after '%s'..." % matching
+
+    generate_matching_boots(config, connection, bundles, folder, matching)
+    
+
+################################################################################
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", help="configuration for the LAVA server")
+    parser.add_argument("--config",   help="configuration for the LAVA server")
     parser.add_argument("--section", default="default", help="section in the LAVA config file")
     parser.add_argument("--username", help="username for the LAVA server")
-    parser.add_argument("--token", help="token for LAVA server api")
-    parser.add_argument("--server", help="server url for LAVA server")
-    parser.add_argument("--stream", help="bundle stream for LAVA server")
+    parser.add_argument("--token",    help="token for LAVA server api")
+    parser.add_argument("--server",   help="server url for LAVA server")
+    parser.add_argument("--stream",   help="bundle stream for LAVA server")
     parser.add_argument("--matching", help="a substring to look for in the job name")
     my_args = vars(parser.parse_args())
     main(my_args)
